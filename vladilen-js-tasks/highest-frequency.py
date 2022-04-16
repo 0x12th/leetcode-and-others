97fd497171e2432a42fcbb7b1e1b1ace7c19ad9a
Nikita Biryukov <n.biryukov12@mail.ru>
2022-04-16 21:12:41 +0300

    New tasks

---
+++ vladilen-js-tasks/highest-frequency.py
@@ -0,0 +1,19 @@
+# ['a', 'b', 'c', 'c', 'd', 'e'] -> c
+# ['abc', 'def', 'abc', 'def', 'abc'] -> abc
+# ['abc', 'def'] -> abc
+# ['abc', 'abc', 'def', 'def', 'def', 'ghi', 'ghi', 'ghi', 'ghi' ] -> ghi
+
+
+import collections
+
+
+def get_highest_freq_str(lst: list[str]) -> str:
+    dct = collections.defaultdict(int)
+    result = ''
+    max_freq = 0
+    for v in lst:
+        dct[v] += 1
+        if dct[v] > max_freq:
+            max_freq = dct[v]
+            result = v
+    return result
---
+++ vladilen-js-tasks/readme.md
@@ -0,0 +1,2 @@
+Ссылка на статью-источник задач:
+https://vladilen.notion.site/JavaScript-473541d86991472fb844c5fa35d5dbf4
