ee5c232b91a0caa037da7bda3233b86de87916c5
Nikita Biryukov <n.biryukov12@gmail.com>
2026-05-05 00:12:29 +0300

    125

---
+++ leetcode-easy/125.py
@@ -0,0 +1,27 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+"""
+
+
+def is_palindrome(s: str) -> bool:
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+            continue
+        if not s[right].isalnum():
+            right -= 1
+            continue
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+
+    return True
